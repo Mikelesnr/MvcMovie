b037c62e9df1c27682547fb8a8671ee4930d430c
Michael Mwanza <mwanza.n.m@gmail.com>
2025-09-24 23:27:49 +0200

    name filter added to movie table

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33fde47fae0cb93a87f29e61279cf529b263852b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e972c778755dc3783702e72809d5addf9b566988")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
