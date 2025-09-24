a912ecc2f6f39ad9d1f3a5edf28425e424d2e517
Michael Mwanza <mwanza.n.m@gmail.com>
2025-09-25 00:13:59 +0200

    validation added

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -25,6 +25,7 @@ public static class SeedData
                     Title = "When Harry Met Sally",
                     ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
+                    Rating = "R",
                     Price = 7.99M
                 },
                 new Movie
@@ -32,6 +33,7 @@ public static class SeedData
                     Title = "Ghostbusters ",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
+                    Rating = "PG",
                     Price = 8.99M
                 },
                 new Movie
@@ -39,6 +41,7 @@ public static class SeedData
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
+                    Rating = "PG",
                     Price = 9.99M
                 },
                 new Movie
@@ -46,7 +49,32 @@ public static class SeedData
                     Title = "Rio Bravo",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
+                    Rating = "PG",
                     Price = 3.99M
+                },
+                new Movie
+                {
+                    Title = "Live and Let Die (James Bond)",
+                    ReleaseDate = DateTime.Parse("1973-1-1"),
+                    Genre = "Action Thriller",
+                    Rating = "PG-13",
+                    Price = 5.99M
+                },
+                new Movie
+                {
+                    Title = "Tomorrow Never Dies (James Bond)",
+                    ReleaseDate = DateTime.Parse("1997-9-12"),
+                    Genre = "Action Thriller",
+                    Rating = "PG-13",
+                    Price = 5.99M
+                },
+                new Movie
+                {
+                    Title = "How to Train Your Dragon",
+                    ReleaseDate = DateTime.Parse("2010-3-26"),
+                    Genre = "Animation",
+                    Rating = "PG",
+                    Price = 6.99M
                 }
             );
             context.SaveChanges();
